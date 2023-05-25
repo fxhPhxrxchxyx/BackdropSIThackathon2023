b21c506fa0fb00177a262ec8f04ddea13b091e73
fah <phurichayakhem1@gmail.com>
2023-05-25 14:58:25 +0700

    time and temp section

--- frontend/src/Pages/Backdrop.tsx
+++ frontend/src/Pages/Backdrop.tsx
@@ -1,17 +1,79 @@
-import { Box } from "@mui/material";
+import { Box, Card, CardMedia, Stack, Typography } from "@mui/material";
 import bg from "../assets/bg.png";
+import { payload } from "../utils/payload";
 const Backdrop = () => {
   return (
-    <Box
-      sx={{
-        width: "1920px",
-        height: "1080px",
-        backgroundImage: `url(${bg})`,
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        filter: "blur(10px)",
-      }}
-    />
+    <Box>
+      <Card>
+        <CardMedia
+          sx={{
+            position: "relative",
+            width: "1920px",
+            height: "1080px",
+            backgroundImage: `url(${bg})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            filter: "blur(10px)",
+          }}
+        />
+        <Stack>
+          <Typography
+            sx={{
+              position: "absolute",
+              top: "8%",
+              width: "100%",
+              left: "5%",
+              fontFamily: "open sans",
+              fontWeight: "bold",
+              textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+            }}
+            variant="h1"
+            color="white"
+          >
+            {payload.time}
+          </Typography>
+          <Typography
+            sx={{
+              position: "absolute",
+              top: "20%",
+              width: "100%",
+              left: "6%",
+              fontFamily: "open sans",
+              textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+            }}
+            variant="h4"
+            color="white"
+          >
+            {payload.date}
+          </Typography>
+          <Stack>
+            <img
+              src={payload.wheather.icon}
+              style={{
+                position: "absolute",
+                top: "10%",
+                left: "20%",
+                width: "3%",
+              }}
+            ></img>
+            <Typography
+              sx={{
+                position: "absolute",
+                top: "15%",
+                width: "100%",
+                left: "20%",
+                fontFamily: "open sans",
+                textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+              }}
+              variant="h6"
+              color="white"
+            >
+              {payload.wheather.temp}° • {payload.wheather.status}
+            </Typography>
+          </Stack>
+        </Stack>
+      </Card>
+    </Box>
   );
 };
 
---
+++ frontend/src/utils/payload.ts
@@ -0,0 +1,25 @@
+export const payload = {
+  time: "10:53",
+  date: "Wed, May 24",
+  wheather: {
+    icon: "http://www.gstatic.com/images/icons/material/apps/weather/2x/mostly_cloudy_night_dark_color_96dp.png",
+    status: "Mostly Cloudy",
+    temp: "55",
+  },
+  current_event: {
+    title: "Launch",
+    time: "12:00 PM - 1:00 PM",
+  },
+  next_event: {
+    title: "Hackathon Session",
+    time: "1:00 PM - 2:30 PM",
+  },
+  now_playing: {
+    cover_url:
+      "https://lh3.googleusercontent.com/D0Ov-18hSEVy5327rmqHfXS-4YDbM4xh6NSLuLGU295Mh2PGTFvKH-g0GllIYkMZsFHXPMeVx5oRHzM=w544-h544-l90-rj",
+    title: "Running Through The Night",
+    artist: "Seori",
+    album: "?depacse ohw",
+    queue_by: "Phurichaya K.",
+  },
+};
