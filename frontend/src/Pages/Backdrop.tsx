c72ed35f79227390cdfb81f57f26fd25918b920c
Thun (MacBook Pro) <bsthun@gmail.com>
2023-05-27 14:25:19 +0700

    asd

--- frontend/src/Pages/Backdrop.tsx
+++ frontend/src/Pages/Backdrop.tsx
@@ -9,223 +9,223 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Backdrop = () => {
-  let queue = Boolean(payload.now_playing.queue_by);
-  const [state, setState] = useState<string>({});
-  const [bg, setBg] = useState<string>(
-    "https://api.cshack.site/api/image/rand"
-  );
-  const fetchImage = () => {
-    setInterval(() => {
-      setBg("https://api.cshack.site/api/image/rand?salt=" + Math.random());
-      console.log(111);
-    }, 10000);
-  };
+	let queue = Boolean(payload.now_playing.queue_by);
+	const [state, setState] = useState<string>({});
+	const [bg, setBg] = useState<string>(
+		"https://api.cshack.site/api/image/rand"
+	);
+	const fetchImage = () => {
+		setInterval(() => {
+			setBg("https://api.cshack.site/api/image/rand?salt=" + Math.random());
+			console.log(111);
+		}, 10000);
+	};
 
-  useEffect(() => {
-    fetchImage();
-    axios.get("https://api.cshack.site/api/backdrop/state").then((res) => {
-      setState(res.data.data);
-    });
-    setInterval(() => {
-      axios.get("https://api.cshack.site/api/backdrop/state").then((res) => {
-        setState(res.data.data);
-      });
-    }, 5000);
-  }, []);
+	useEffect(() => {
+		fetchImage();
+		axios.get("https://api.cshack.site/api/backdrop/state").then((res) => {
+			setState(res.data.data);
+		});
+		setInterval(() => {
+			axios.get("https://api.cshack.site/api/backdrop/state").then((res) => {
+				setState(res.data.data);
+			});
+		}, 5000);
+	}, []);
 
-  return (
-    <Box>
-      <Card>
-        <CardMedia
-          sx={{
-            position: "relative",
-            width: "100%",
-            height: "100vh",
-            backgroundImage: `url(${bg})`,
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            filter: "blur(4px)",
-          }}
-        />
-        <div
-          style={{
-            background: "rgba(0, 0, 0, 0.25)",
-            position: "absolute",
-            inset: "0px",
-          }}
-        ></div>
-        <Stack>
-          <Typography
-            sx={{
-              position: "absolute",
-              top: "50px",
-              width: "100%",
-              left: "90px",
-              fontFamily: "open sans",
-              fontWeight: "bold",
-              textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-              fontSize: "150px",
-            }}
-            color="white"
-          >
-            {state.time}
-          </Typography>
-          <Typography
-            sx={{
-              position: "absolute",
-              top: "240px",
-              width: "100%",
-              left: "90px",
-              fontFamily: "open sans",
-              textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-            }}
-            variant="h3"
-            color="white"
-          >
-            {state.date}
-          </Typography>
-          <Stack>
-            <img
-              src={state.whether?.icon}
-              style={{
-                position: "absolute",
-                top: "120px",
-                left: "520px",
-              }}
-            ></img>
-            <Typography
-              sx={{
-                position: "absolute",
-                top: "180px",
-                width: "100%",
-                left: "520px",
-                fontFamily: "open sans",
-                textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-              }}
-              variant="h4"
-              color="white"
-            >
-              {state.whether?.temp}° • {state.whether?.status}
-            </Typography>
-          </Stack>
-        </Stack>
-        <Stack>
-          <div
-            style={{
-              position: "absolute",
-              top: "340px",
-              left: "108px",
-              width: "6px",
-              height: "105px",
-              background: "white",
-              borderRadius: "10px",
-            }}
-          ></div>
-          <Stack>
-            <Typography
-              sx={{
-                position: "absolute",
-                top: "340px",
-                left: "130px",
-              }}
-              variant="h3"
-              color={"white"}
-            >
-              {state.current_event?.title}
-            </Typography>
-            <Typography
-              sx={{ position: "absolute", top: "400px", left: "130px" }}
-              variant="h4"
-              color={"white"}
-            >
-              {state.current_event?.time}
-            </Typography>
-          </Stack>
-        </Stack>
-        <Stack>
-          <div
-            style={{
-              position: "absolute",
-              top: "480px",
-              left: "108px",
-              width: "6px",
-              height: "105px",
-              background: "white",
-              borderRadius: "10px",
-            }}
-          ></div>
-          <Stack>
-            <Typography
-              sx={{
-                position: "absolute",
-                top: "480px",
-                left: "130px",
-                opacity: 0.8,
-              }}
-              variant="h5"
-              color={"white"}
-            >
-              Up next
-            </Typography>
-            <Typography
-              sx={{
-                position: "absolute",
-                top: "515px",
-                left: "130px",
-              }}
-              variant="h5"
-              color={"white"}
-            >
-              {state.next_event?.title}
-            </Typography>
-            <Typography
-              sx={{
-                position: "absolute",
-                top: "550px",
-                left: "130px",
-              }}
-              variant="h6"
-              color={"white"}
-            >
-              {state.next_event?.time}
-            </Typography>
-            <img
-              style={{
-                position: "absolute",
-                top: "665px",
-                left: "122px",
-                width: "85px",
-                height: "85px",
-              }}
-              className="lightMove"
-              src={light2}
-            ></img>
-            <img
-              style={{
-                position: "absolute",
-                top: "680px",
-                left: "135px",
-                width: "60px",
-                height: "60px",
-              }}
-              className="lightMove"
-              src={light1}
-            ></img>
-            <img
-              style={{
-                position: "absolute",
-                top: "620px",
-                left: "140px",
-                width: "110px",
-                height: "110px",
-              }}
-              src={logo}
-            ></img>
-          </Stack>
-        </Stack>
-        <Box sx={{ position: "absolute", bottom: 320, right: 170 }}>
-          <Typography sx={{ color: "white", fontSize: 20, opacity: 1 }}>
-            {/* <span style={{ opacity: 0.7 }}>You can request music at</span>{" "}
+	return (
+		<Box>
+			<Card>
+				<CardMedia
+					sx={{
+						position: "relative",
+						width: "100%",
+						height: "100vh",
+						backgroundImage: `url(${bg})`,
+						backgroundSize: "cover",
+						backgroundRepeat: "no-repeat",
+						filter: "blur(1px)",
+					}}
+				/>
+				<div
+					style={{
+						background: "rgba(0, 0, 0, 0.25)",
+						position: "absolute",
+						inset: "0px",
+					}}
+				></div>
+				<Stack>
+					<Typography
+						sx={{
+							position: "absolute",
+							top: "50px",
+							width: "100%",
+							left: "90px",
+							fontFamily: "open sans",
+							fontWeight: "bold",
+							textShadow: "0px 4px 4px rgba(0, 0, 0, 0.2)",
+							fontSize: "150px",
+						}}
+						color="white"
+					>
+						{state.time}
+					</Typography>
+					<Typography
+						sx={{
+							position: "absolute",
+							top: "240px",
+							width: "100%",
+							left: "90px",
+							fontFamily: "open sans",
+							textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+						}}
+						variant="h3"
+						color="white"
+					>
+						{state.date}
+					</Typography>
+					<Stack>
+						<img
+							src={state.whether?.icon}
+							style={{
+								position: "absolute",
+								top: "120px",
+								left: "520px",
+							}}
+						></img>
+						<Typography
+							sx={{
+								position: "absolute",
+								top: "180px",
+								width: "100%",
+								left: "520px",
+								fontFamily: "open sans",
+								textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+							}}
+							variant="h4"
+							color="white"
+						>
+							{state.whether?.temp}° • {state.whether?.status}
+						</Typography>
+					</Stack>
+				</Stack>
+				<Stack>
+					<div
+						style={{
+							position: "absolute",
+							top: "340px",
+							left: "108px",
+							width: "6px",
+							height: "105px",
+							background: "white",
+							borderRadius: "10px",
+						}}
+					></div>
+					<Stack>
+						<Typography
+							sx={{
+								position: "absolute",
+								top: "340px",
+								left: "130px",
+							}}
+							variant="h3"
+							color={"white"}
+						>
+							{state.current_event?.title}
+						</Typography>
+						<Typography
+							sx={{ position: "absolute", top: "400px", left: "130px" }}
+							variant="h4"
+							color={"white"}
+						>
+							{state.current_event?.time}
+						</Typography>
+					</Stack>
+				</Stack>
+				<Stack>
+					<div
+						style={{
+							position: "absolute",
+							top: "480px",
+							left: "108px",
+							width: "6px",
+							height: "105px",
+							background: "white",
+							borderRadius: "10px",
+						}}
+					></div>
+					<Stack>
+						<Typography
+							sx={{
+								position: "absolute",
+								top: "480px",
+								left: "130px",
+								opacity: 0.8,
+							}}
+							variant="h5"
+							color={"white"}
+						>
+							Up next
+						</Typography>
+						<Typography
+							sx={{
+								position: "absolute",
+								top: "515px",
+								left: "130px",
+							}}
+							variant="h5"
+							color={"white"}
+						>
+							{state.next_event?.title}
+						</Typography>
+						<Typography
+							sx={{
+								position: "absolute",
+								top: "550px",
+								left: "130px",
+							}}
+							variant="h6"
+							color={"white"}
+						>
+							{state.next_event?.time}
+						</Typography>
+						<img
+							style={{
+								position: "absolute",
+								top: "665px",
+								left: "122px",
+								width: "85px",
+								height: "85px",
+							}}
+							className="lightMove"
+							src={light2}
+						></img>
+						<img
+							style={{
+								position: "absolute",
+								top: "680px",
+								left: "135px",
+								width: "60px",
+								height: "60px",
+							}}
+							className="lightMove"
+							src={light1}
+						></img>
+						<img
+							style={{
+								position: "absolute",
+								top: "620px",
+								left: "140px",
+								width: "110px",
+								height: "110px",
+							}}
+							src={logo}
+						></img>
+					</Stack>
+				</Stack>
+				<Box sx={{ position: "absolute", bottom: 320, right: 170 }}>
+					<Typography sx={{ color: "white", fontSize: 20, opacity: 1 }}>
+						{/* <span style={{ opacity: 0.7 }}>You can request music at</span>{" "}
             <span
               style={{
                 paddingBottom: "5px",
@@ -234,101 +234,101 @@ const Backdrop = () => {
             >
               music.cshack.site
             </span> */}
-            <Box sx={{ textAlign: "center" }}>
-              <Box sx={{fontSize:"32px"}}>CS hackratron Service</Box>
-              <Box sx={{backgroundColor:"white",padding:"10px 0px"}}>
-                <img
-                  src={qr}
-                  style={{ height: "300px", background: "color" }}
-                ></img>
-              </Box>
-            </Box>
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            width: "500px",
-            height: "180px",
-            position: "absolute",
-            bottom: "80px",
-            right: "90px",
-            background: "rgba(0, 0, 0, 0.25)",
-            borderRadius: "10px",
-            display: "flex",
-            flexDirection: "column",
-            padding: "15px 20px",
-          }}
-        >
-          <Typography variant="h5" color={"white"} mb="10px">
-            Now Playing
-          </Typography>
-          <Stack direction="row" gap="18px">
-            <Box
-              sx={{
-                width: "110px",
-                height: "110px",
-              }}
-            >
-              <img
-                src={state.now_playing?.cover_url}
-                style={{
-                  width: "100%",
-                  backgroundSize: "cover",
-                  borderRadius: "15px",
-                }}
-              ></img>
-            </Box>
-            <Stack direction="column">
-              <Typography
-                variant="h5"
-                color={"white"}
-                sx={{
-                  width: "350px",
-                  whiteSpace: "nowrap",
-                  overflow: "hidden",
-                  textOverflow: "ellipsis",
-                  paddingRight: "10px",
-                }}
-              >
-                {state.now_playing?.title.length > 20 ? (
-                  <Marquee>
-                    {state.now_playing?.title}
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                  </Marquee>
-                ) : (
-                  state.now_playing?.title
-                )}
-              </Typography>
+						<Box sx={{ textAlign: "center" }}>
+							<Box sx={{ fontSize: "32px" }}>CS hackratron Service</Box>
+							<Box sx={{ backgroundColor: "white", padding: "10px 0px" }}>
+								<img
+									src={qr}
+									style={{ height: "300px", background: "color" }}
+								></img>
+							</Box>
+						</Box>
+					</Typography>
+				</Box>
+				<Box
+					sx={{
+						width: "500px",
+						height: "180px",
+						position: "absolute",
+						bottom: "80px",
+						right: "90px",
+						background: "rgba(0, 0, 0, 0.25)",
+						borderRadius: "10px",
+						display: "flex",
+						flexDirection: "column",
+						padding: "15px 20px",
+					}}
+				>
+					<Typography variant="h5" color={"white"} mb="10px">
+						Now Playing
+					</Typography>
+					<Stack direction="row" gap="18px">
+						<Box
+							sx={{
+								width: "110px",
+								height: "110px",
+							}}
+						>
+							<img
+								src={state.now_playing?.cover_url}
+								style={{
+									width: "100%",
+									backgroundSize: "cover",
+									borderRadius: "15px",
+								}}
+							></img>
+						</Box>
+						<Stack direction="column">
+							<Typography
+								variant="h5"
+								color={"white"}
+								sx={{
+									width: "350px",
+									whiteSpace: "nowrap",
+									overflow: "hidden",
+									textOverflow: "ellipsis",
+									paddingRight: "10px",
+								}}
+							>
+								{state.now_playing?.title.length > 20 ? (
+									<Marquee>
+										{state.now_playing?.title}
+										&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+									</Marquee>
+								) : (
+									state.now_playing?.title
+								)}
+							</Typography>
 
-              <Typography variant="h6" color={"white"}>
-                {state.now_playing?.artist}
-              </Typography>
-              <Stack direction="row" gap="10px">
-                <Typography variant="h6" color={"#D1D1D1"}>
-                  {state.now_playing?.queue_by ? "Queued by" : "From Radio"}
-                </Typography>
-                <Stack>
-                  <Typography
-                    variant="h6"
-                    color={"white"}
-                    style={{
-                      whiteSpace: "nowrap",
-                    }}
-                  >
-                    {state.now_playing?.queue_by
-                      ? `${state.now_playing?.queue_by.substring(0, 20)}${
-                          state.now_playing?.queue_by.length > 20 ? "..." : ""
-                        }`
-                      : ""}
-                  </Typography>
-                </Stack>
-              </Stack>
-            </Stack>
-          </Stack>
-        </Box>
-      </Card>
-    </Box>
-  );
+							<Typography variant="h6" color={"white"}>
+								{state.now_playing?.artist}
+							</Typography>
+							<Stack direction="row" gap="10px">
+								<Typography variant="h6" color={"#D1D1D1"}>
+									{state.now_playing?.queue_by ? "Queued by" : "From Radio"}
+								</Typography>
+								<Stack>
+									<Typography
+										variant="h6"
+										color={"white"}
+										style={{
+											whiteSpace: "nowrap",
+										}}
+									>
+										{state.now_playing?.queue_by
+											? `${state.now_playing?.queue_by.substring(0, 20)}${
+													state.now_playing?.queue_by.length > 20 ? "..." : ""
+											  }`
+											: ""}
+									</Typography>
+								</Stack>
+							</Stack>
+						</Stack>
+					</Stack>
+				</Box>
+			</Card>
+		</Box>
+	);
 };
 
 export default Backdrop;
--- frontend/src/Pages/Countdown.tsx
+++ frontend/src/Pages/Countdown.tsx
@@ -4,112 +4,112 @@ import { useEffect, useState } from "react";
 import logo from "../assets/logo.png";
 
 interface Time {
-  hour: number;
-  minute: number;
-  second: number;
+	hour: number;
+	minute: number;
+	second: number;
 }
 
 const Countdown = () => {
-  const [bg, setBg] = useState<string>(
-    "https://api.cshack.site/api/image/rand"
-  );
-  const [remainingTime, setRemainingTime] = useState<Time>({
-    hour: 0,
-    minute: 0,
-    second: 0,
-  });
+	const [bg, setBg] = useState<string>(
+		"https://api.cshack.site/api/image/rand"
+	);
+	const [remainingTime, setRemainingTime] = useState<Time>({
+		hour: 0,
+		minute: 0,
+		second: 0,
+	});
 
-  const initializeCountdownTimer = () => {
-    const currentTimestamp = new Date().getTime();
-    const deadlineTimestamp = new Date("2023-05-28T13:30:00").getTime();
-    let remainingTimeInSecond = Math.floor(
-      (deadlineTimestamp - currentTimestamp) / 1000
-    );
-    const timer = setInterval(() => {
-      setRemainingTime({
-        hour: Math.floor(remainingTimeInSecond / 3600),
-        minute: Math.floor((remainingTimeInSecond % 3600) / 60),
-        second: remainingTimeInSecond % 60,
-      });
+	const initializeCountdownTimer = () => {
+		const currentTimestamp = new Date().getTime();
+		const deadlineTimestamp = new Date("2023-05-28T13:30:00").getTime();
+		let remainingTimeInSecond = Math.floor(
+			(deadlineTimestamp - currentTimestamp) / 1000
+		);
+		const timer = setInterval(() => {
+			setRemainingTime({
+				hour: Math.floor(remainingTimeInSecond / 3600),
+				minute: Math.floor((remainingTimeInSecond % 3600) / 60),
+				second: remainingTimeInSecond % 60,
+			});
 
-      if (remainingTimeInSecond <= 0) {
-        clearInterval(timer);
-        console.log("Countdown complete!");
-      }
-      remainingTimeInSecond -= 1;
-    }, 1000);
-    return timer;
-  };
-  const fetchImage = () => {
-    console.log(111);
-    
-    setInterval(() => {
-      setBg("https://api.cshack.site/api/image/rand?salt=" + Math.random());
-    }, 10000);
-  };
+			if (remainingTimeInSecond <= 0) {
+				clearInterval(timer);
+				console.log("Countdown complete!");
+			}
+			remainingTimeInSecond -= 1;
+		}, 1000);
+		return timer;
+	};
+	const fetchImage = () => {
+		console.log(111);
 
-  useEffect(() => {
-    fetchImage();
-    const timer = initializeCountdownTimer();
-    return () => clearInterval(timer);
-  }, []);
+		setInterval(() => {
+			setBg("https://api.cshack.site/api/image/rand?salt=" + Math.random());
+		}, 10000);
+	};
 
-  return (
-    <Box
-      sx={{
-        width: "100vw",
-        height: "100vh",
-        backgroundImage: `url(${bg})`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        "&::after": {
-          content: '""',
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-          backgroundColor: "rgba(0, 0, 0, 0.5)", // Adjust the color and opacity as needed
-          zIndex: "1",
-        },
-      }}
-    >
-      <Container sx={{ height: "100%" }}>
-        <Stack
-          justifyContent="center"
-          alignItems={"center"}
-          sx={{ height: "100%" }}
-        >
-          <Typography
-            sx={{
-              color: "white",
-              position: "relative",
-              top: "3rem",
-              zIndex: "100",
-            }}
-            variant="h4"
-          >
-            Hackathon Period Remaining
-          </Typography>
-          <Stack direction={"row"}>
-            <Time text={remainingTime.hour} unit="Hours" />
-            <Time text={remainingTime.minute} unit="Minutes" />
-            <Time text={remainingTime.second} unit="Seconds" />
-          </Stack>
-          <Stack direction={"row"} alignItems={"center"}>
-            <Avatar
-              alt="SIT Hackathon logo"
-              src={logo}
-              sx={{ width: 150, height: 150, zIndex: "100" }}
-            />
-            <Typography sx={{ color: "white", zIndex: "100" }} variant="h5">
-              SIT Hackathon 2023
-            </Typography>
-          </Stack>
-        </Stack>
-      </Container>
-    </Box>
-  );
+	useEffect(() => {
+		fetchImage();
+		const timer = initializeCountdownTimer();
+		return () => clearInterval(timer);
+	}, []);
+
+	return (
+		<Box
+			sx={{
+				width: "100vw",
+				height: "100vh",
+				backgroundImage: `url(${bg})`,
+				backgroundRepeat: "no-repeat",
+				backgroundSize: "cover",
+				"&::after": {
+					content: '""',
+					position: "absolute",
+					top: 0,
+					left: 0,
+					width: "100%",
+					height: "100%",
+					backgroundColor: "rgba(0, 0, 0, 0.2)", // Adjust the color and opacity as needed
+					zIndex: "1",
+				},
+			}}
+		>
+			<Container sx={{ height: "100%" }}>
+				<Stack
+					justifyContent="center"
+					alignItems={"center"}
+					sx={{ height: "100%" }}
+				>
+					<Typography
+						sx={{
+							color: "white",
+							position: "relative",
+							top: "3rem",
+							zIndex: "100",
+						}}
+						variant="h4"
+					>
+						Hackathon Period Remaining
+					</Typography>
+					<Stack direction={"row"}>
+						<Time text={remainingTime.hour} unit="Hours" />
+						<Time text={remainingTime.minute} unit="Minutes" />
+						<Time text={remainingTime.second} unit="Seconds" />
+					</Stack>
+					<Stack direction={"row"} alignItems={"center"}>
+						<Avatar
+							alt="SIT Hackathon logo"
+							src={logo}
+							sx={{ width: 150, height: 150, zIndex: "100" }}
+						/>
+						<Typography sx={{ color: "white", zIndex: "100" }} variant="h5">
+							SIT Hackathon 2023
+						</Typography>
+					</Stack>
+				</Stack>
+			</Container>
+		</Box>
+	);
 };
 
 export default Countdown;
